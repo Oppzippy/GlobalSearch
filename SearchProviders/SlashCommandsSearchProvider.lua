dc469e1491680b8ee4d0ef593973d182b8078e04
Kyle Scheuing <kyle@kylescheuing.com>
2022-06-30 18:11:44 -0400

    Minor performance improvement for SlashCommandsSearchProvider
    Around 1-2% improvement. This is by far the slowest provider, so it
    does matter to an extent.

--- SearchProviders/SlashCommandsSearchProvider.lua
+++ SearchProviders/SlashCommandsSearchProvider.lua
@@ -26,10 +26,11 @@ function SlashCommandsSearchProvider:Fetch()
 	-- SlashCmdList seems to get cleared somewhat randomly (10-15 sec after
 	-- /reload or so), so it's not reliable to use that as a source of slash
 	-- commands.
+	local type, strfind = type, string.find
 	for k, v in next, _G do
-		if type(k) == "string" and k:find("^SLASH_.+%d+$") == 1 then
+		if type(k) == "string" and strfind(k, "^SLASH_.+%d+$") then
 			-- Filter out SLASH_TEXTTOSPEECH and other non-slash commands with the prefix
-			if type(v) == "string" and v:find("/", nil, true) == 1 then
+			if type(v) == "string" and strfind(k, "/", nil, true) == 1 then
 				commands[v] = true
 			end
 		end
