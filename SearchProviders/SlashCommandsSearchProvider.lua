629ccf75563fb951e2442ce77763bb00225d3aaa
Kyle Scheuing <kyle@kylescheuing.com>
2022-06-30 17:51:41 -0400

    Improve SlashCommandsSearchProvider performance
    We don't need to search the whole string for SLASH_, since we require it
    to start with that anyway.

--- SearchProviders/SlashCommandsSearchProvider.lua
+++ SearchProviders/SlashCommandsSearchProvider.lua
@@ -27,7 +27,7 @@ function SlashCommandsSearchProvider:Fetch()
 	-- /reload or so), so it's not reliable to use that as a source of slash
 	-- commands.
 	for k, v in next, _G do
-		if type(k) == "string" and k:find("SLASH_.+%d+") == 1 then
+		if type(k) == "string" and k:find("^SLASH_.+%d+$") == 1 then
 			-- Filter out SLASH_TEXTTOSPEECH and other non-slash commands with the prefix
 			if type(v) == "string" and v:find("/", nil, true) == 1 then
 				commands[v] = true
