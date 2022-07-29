9130462e9dffa6051b3d305f7c5209ccf13f060f
Kyle Scheuing <kyle@kylescheuing.com>
2022-07-28 23:56:53 -0400

    Include all achievements in chains
    Before, only the achievement with its own listing in the achievement ui
    would be listed. Now, the previous/next achievements will be listed as
    well.

--- SearchProviders/AchievementsProvider.lua
+++ SearchProviders/AchievementsProvider.lua
@@ -32,23 +32,28 @@ function AchievementsSearchProvider:Fetch()
 			end,
 		}
 	end
+	print(#items)
 
 	return items
 end
 
 function AchievementsSearchProvider:IterateAchievements()
 	local categoryIDs = GetCategoryList()
+	local numCategories = #categoryIDs
 	local categoryIndex = 1
 	local achievementIndex = 0
-	local achievementId
+
+	local siblingAchievementIterator
+
+	local GetAchievementInfo, GetCategoryNumAchievements = GetAchievementInfo,
+		GetCategoryNumAchievements
 	return function()
-		if achievementId then
-			local previousAchievementID = GetPreviousAchievement(achievementId)
-			if previousAchievementID then
-				local achievement = { GetAchievementInfo(previousAchievementID) }
-				achievementId = previousAchievementID
+		if siblingAchievementIterator then
+			local achievement = siblingAchievementIterator()
+			if achievement then
 				return achievement
 			end
+			siblingAchievementIterator = nil
 		end
 
 		achievementIndex = achievementIndex + 1
@@ -56,14 +61,50 @@ function AchievementsSearchProvider:IterateAchievements()
 			achievementIndex = 1
 			repeat
 				categoryIndex = categoryIndex + 1
-			until categoryIndex > #categoryIDs or GetCategoryNumAchievements(categoryIDs[categoryIndex], false) ~= 0
-			if categoryIndex > #categoryIDs then
+			until categoryIndex > numCategories or GetCategoryNumAchievements(categoryIDs[categoryIndex], false) ~= 0
+			if categoryIndex > numCategories then
 				return
 			end
 		end
 		local achievement = { GetAchievementInfo(categoryIDs[categoryIndex], achievementIndex) }
-		achievementId = achievement[1]
+		siblingAchievementIterator = self:IterateSiblingAchievements(achievement[1])
 		return achievement
+	end
+end
+
+function AchievementsSearchProvider:IterateSiblingAchievements(achievementId)
+	local iterator = self:IterateNextAchievements(achievementId)
+	local stage = 1
+	return function()
+		local achievement = iterator()
+		if not achievement then
+			if stage == 1 then
+				iterator = self:IteratePreviousAchievements(achievementId)
+				stage = 2
+				return iterator()
+			else
+				return
+			end
+		end
+		return achievement
+	end
+end
+
+function AchievementsSearchProvider:IteratePreviousAchievements(achievementId)
+	return function()
+		achievementId = GetPreviousAchievement(achievementId)
+		if achievementId then
+			return { GetAchievementInfo(achievementId) }
+		end
+	end
+end
+
+function AchievementsSearchProvider:IterateNextAchievements(achievementId)
+	return function()
+		achievementId = GetNextAchievement(achievementId)
+		if achievementId then
+			return { GetAchievementInfo(achievementId) }
+		end
 	end
 end
 
