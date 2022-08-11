7493709a9ab7ac988f407b3e42b18e865874edad
Kyle Scheuing <kyle@kylescheuing.com>
2022-08-11 01:18:08 -0400

    Group together match ranges when possible

--- Internal/QueryMatcher.lua
+++ Internal/QueryMatcher.lua
@@ -5,6 +5,44 @@ local ns = select(2, ...)
 ---@field from integer
 ---@field to integer
 
+--- Attempts to group together match ranges when the a match range could exist in multiple places
+--- Example:
+--- Crusader Strike
+--- Search "Strike", result selects "Cru*s*ader S*trike*"
+--- This could instead be "Crusader *Strike*"
+---@param text string
+---@param matchRanges MatchRange[]
+---@return MatchRange[]
+local function CondenseMatchRanges(text, matchRanges)
+	local rangeStack = {}
+	for i = #matchRanges, 1, -1 do
+		rangeStack[#rangeStack + 1] = matchRanges[i]
+	end
+
+	local newRanges = {}
+	while #rangeStack > 1 do
+		local currentRange = rangeStack[#rangeStack]
+		rangeStack[#rangeStack] = nil
+		local nextRange = rangeStack[#rangeStack]
+
+		local currentString = text:sub(currentRange.from, currentRange.to)
+		local stringLength = #currentString
+		local nextStringPrefix = text:sub(nextRange.from - stringLength, nextRange.from - 1)
+		if currentString == nextStringPrefix then
+			-- The new range is added to the stack so it can be merged with even more ranges
+			rangeStack[#rangeStack] = {
+				from = nextRange.from - stringLength,
+				to = nextRange.to,
+			}
+		else
+			newRanges[#newRanges + 1] = currentRange
+		end
+	end
+	newRanges[#newRanges + 1] = rangeStack[1]
+
+	return newRanges
+end
+
 ---@param query string
 ---@param text string
 ---@return boolean, MatchRange[]?
@@ -36,6 +74,9 @@ local function MatchesQuery(query, text)
 		end
 		prevIndex = index
 	end
+
+	matchRanges = CondenseMatchRanges(text, matchRanges)
+
 	return true, matchRanges
 end
 
