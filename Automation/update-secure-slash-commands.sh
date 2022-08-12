7f97bb66ca48cc464ed51d8e82b5eda1dd89a0cf
Kyle Scheuing <kyle@kylescheuing.com>
2022-08-12 05:16:08 -0400

    Add generated list of secure slash commands

---
+++ Automation/update-secure-slash-commands.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+get_slash_commands_from_url() {
+	echo "{"
+	curl -s -S "$1" | sed -n -E "s/^.*SecureCmdList\[\"(\w+)\"] *= *function.*$/\t\"\1\",/p"
+	echo "}"
+}
+
+get_slash_commands_for_all_versions() {
+	declare -A urls
+	urls["\"default\""]="https://raw.githubusercontent.com/BigWigsMods/WoWUI/live/FrameXML/ChatFrame.lua"
+	urls["0"]="https://raw.githubusercontent.com/BigWigsMods/WoWUI/vanilla/Interface/FrameXML/ChatFrame.lua"
+	urls["1"]="https://raw.githubusercontent.com/BigWigsMods/WoWUI/tbc/Interface/FrameXML/ChatFrame.lua"
+
+	for key in "${!urls[@]}"; do
+		echo ""
+		echo -n "ns.SecureSlashCommandLists[$key] = "
+		get_slash_commands_from_url "${urls[$key]}"
+	done
+}
+
+cat << EOF
+---@class ns
+local ns = select(2, ...)
+ns.SecureSlashCommandLists = {}
+EOF
+get_slash_commands_for_all_versions
