f4c9ad7e35b2bcda8bbb2242e1ec50a7bc67e676
Kyle Scheuing <kyle@kylescheuing.com>
2022-08-12 03:33:58 -0400

    Fetch slash commands from hash_SlashCmdList
    Scanning _G will pick up some more command such as everything in
    SecureCmdList, but it's much slower. In the future, maybe package a list
    of everything in SecureCmdList with the addon.

--- SearchProviders/SlashCommandsSearchProvider.lua
+++ SearchProviders/SlashCommandsSearchProvider.lua
@@ -19,25 +19,16 @@ end
 
 ---@return SearchItem[]
 function SlashCommandsSearchProvider:Fetch()
-	local items = {}
-
+	ChatFrame_ImportAllListsToHash()
 	local commands = {}
-	-- SlashCmdList seems to get cleared somewhat randomly (10-15 sec after
-	-- /reload or so), so it's not reliable to use that as a source of slash
-	-- commands.
-	local type, strfind = type, string.find
-	for key, value in next, _G do
-		if type(key) == "string" and strfind(key, "^SLASH_.+%d+$") then
-			-- Filter out SLASH_TEXTTOSPEECH and other non-slash commands with the prefix
-			if type(value) == "string" and strfind(value, "/", nil, true) == 1 then
-				commands[value] = true
-			end
-		end
+	for command in next, hash_SlashCmdList do
+		commands[command] = true
 	end
 
+	local items = {}
 	for command in next, commands do
 		items[#items + 1] = {
-			name = command,
+			name = command:lower(),
 			category = L.slash_commands,
 			texture = 136243, -- Interface/Icons/Trade_Engineering
 			macroText = command,
