1fd4f5e438586ee941fb574dfc89876b94957e7a
Kyle Scheuing <kyle@kylescheuing.com>
2022-08-28 03:36:05 -0400

    Add InstanceOptionsSearchProvider

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -29,6 +29,8 @@ L.boss_from_instance = "%s (%s)"
 L.minimap_tracking = "Minimap Tracking"
 L.x_is_enabled = "%s is enabled."
 L.x_is_disabled = "%s is disabled."
+L.dungeon_difficulty_x = "Dungeon Difficulty: %s"
+L.raid_difficulty_x = "Raid Difficulty: %s"
 
 L.achievements_search_provider_desc = "Provides all achievements. Includes earned, unearned, and achievements further in chains that aren't visible in the UI yet."
 L.bags_search_provider_desc = "Provides all usable items in bags."
@@ -41,6 +43,7 @@ L.spells_search_provider = "Provides all usable spells."
 L.system_options_search_provider = "Provides all options from Game Menu > System."
 L.toys_search_provider = "Provides all known toys."
 L.ui_panels_search_provider = "Provides various UI panels from the base UI."
+L.legacy_raid_difficulty_x = "Legacy Raid Difficulty: %s"
 
 L.key_bindings = KEY_BINDINGS
 L.general = GENERAL
@@ -62,3 +65,5 @@ L.spells = SPELLS
 L.macros = MACROS
 L.interface_options = INTERFACE_OPTIONS
 L.encounter_journal = ENCOUNTER_JOURNAL
+L.instance_options = UNIT_FRAME_DROPDOWN_SUBSECTION_TITLE_INSTANCE
+L.reset_all_instances = RESET_INSTANCES
---
+++ SearchProviders/InstanceOptionsSearchProvider.lua
@@ -0,0 +1,79 @@
+---@class ns
+local ns = select(2, ...)
+
+local AceLocale = LibStub("AceLocale-3.0")
+local L = AceLocale:GetLocale("GlobalSearch")
+
+---@class InstanceOptionsSearchProvider : SearchProvider
+local InstanceOptionsSearchProvider = {
+	localizedName = L.instance_options,
+}
+
+---@return SearchItem[]
+function InstanceOptionsSearchProvider:Get()
+	if not self.cache then
+		self.cache = self:Fetch()
+	end
+	return self.cache
+end
+
+local dungeonDifficulties = {
+	[1] = PLAYER_DIFFICULTY1, -- Normal
+	[2] = PLAYER_DIFFICULTY2, -- Heroic
+	[23] = PLAYER_DIFFICULTY6, -- Mythic
+}
+
+local raidDifficulties = {
+	[14] = PLAYER_DIFFICULTY1, -- Normal
+	[15] = PLAYER_DIFFICULTY2, -- Heroic
+	[16] = PLAYER_DIFFICULTY6, -- Mythic
+}
+
+---@return SearchItem[]
+function InstanceOptionsSearchProvider:Fetch()
+	---@type SearchItem[]
+	local items = {
+		{
+			name = L.reset_all_instances,
+			texture = 337500, -- Interface/LFGFrame/UI-LFG-PORTRAIT
+			action = function()
+				StaticPopup_Show("CONFIRM_RESET_INSTANCES")
+			end,
+		},
+		{
+			name = L.legacy_raid_difficulty_x:format(RAID_DIFFICULTY1),
+			texture = 341547, -- Interface/LFGFrame/UI-LFR-PORTRAIT
+			action = function()
+				SetLegacyRaidDifficultyID(3)
+			end,
+		},
+		{
+			name = L.legacy_raid_difficulty_x:format(RAID_DIFFICULTY2),
+			texture = 341547, -- Interface/LFGFrame/UI-LFR-PORTRAIT
+			action = function()
+				SetLegacyRaidDifficultyID(4)
+			end,
+		},
+	}
+	for difficultyID, localizedName in next, dungeonDifficulties do
+		items[#items + 1] = {
+			name = L.dungeon_difficulty_x:format(localizedName),
+			texture = 133076, -- Interface/Icons/INV_Helmet_08 (Dungeon Finder icon)
+			action = function()
+				SetDungeonDifficultyID(difficultyID)
+			end,
+		}
+	end
+	for difficultyID, localizedName in next, raidDifficulties do
+		items[#items + 1] = {
+			name = L.raid_difficulty_x:format(localizedName),
+			texture = 341547, -- Interface/LFGFrame/UI-LFR-PORTRAIT
+			action = function()
+				SetRaidDifficultyID(difficultyID)
+			end,
+		}
+	end
+	return items
+end
+
+GlobalSearchAPI:RegisterProvider("GlobalSearch_InstanceOptions", InstanceOptionsSearchProvider)
