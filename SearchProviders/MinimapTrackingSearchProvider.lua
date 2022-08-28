1f164d773bb5bfa0fde75b73d80f38e0d6998807
Kyle Scheuing <kyle@kylescheuing.com>
2022-08-28 02:48:47 -0400

    Add MinimapTrackingSearchProvider

---
+++ SearchProviders/MinimapTrackingSearchProvider.lua
@@ -0,0 +1,49 @@
+---@class ns
+local ns = select(2, ...)
+
+local AceLocale = LibStub("AceLocale-3.0")
+local L = AceLocale:GetLocale("GlobalSearch")
+
+---@class MinimapTrackingSearchProvider : SearchProvider
+local MinimapTrackingSearchProvider = {
+	localizedName = L.minimap_tracking,
+}
+
+---@return SearchItem[]
+function MinimapTrackingSearchProvider:Get()
+	if not self.cache then
+		self.cache = self:Fetch()
+	end
+	return self.cache
+end
+
+---@return SearchItem[]
+function MinimapTrackingSearchProvider:Fetch()
+	---@type SearchItem[]
+	local items = {}
+	for i = 1, GetNumTrackingTypes() do
+		local name, texture = GetTrackingInfo(i)
+		items[i] = {
+			name = name,
+			texture = texture,
+			action = function()
+				local _, _, active = GetTrackingInfo(i)
+				SetTracking(i, not active)
+			end,
+			-- The items are cached so tooltip needs to be a function in order to be updated when
+			-- toggling tracking.
+			---@param tooltip GameTooltip
+			tooltip = function(tooltip)
+				local _, _, active = GetTrackingInfo(i)
+				if active then
+					tooltip:SetText(L.x_is_enabled:format(name))
+				else
+					tooltip:SetText(L.x_is_disabled:format(name))
+				end
+			end
+		}
+	end
+	return items
+end
+
+GlobalSearchAPI:RegisterProvider("GlobalSearch_MinimapTracking", MinimapTrackingSearchProvider)
