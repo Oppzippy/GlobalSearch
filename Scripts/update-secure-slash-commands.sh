9e39ac08ec064488f0cdf5c6b73567a045af4c79
Kyle Scheuing <kyle@kylescheuing.com>
2022-08-31 03:35:45 -0400

    Add wrath secure slash commands

--- Scripts/update-secure-slash-commands.sh
+++ Scripts/update-secure-slash-commands.sh
@@ -13,6 +13,8 @@ get_slash_commands_for_all_versions() {
 	urls["\"default\""]="https://raw.githubusercontent.com/BigWigsMods/WoWUI/live/FrameXML/ChatFrame.lua"
 	urls["0"]="https://raw.githubusercontent.com/BigWigsMods/WoWUI/vanilla/Interface/FrameXML/ChatFrame.lua"
 	urls["1"]="https://raw.githubusercontent.com/BigWigsMods/WoWUI/tbc/Interface/FrameXML/ChatFrame.lua"
+	# TODO change to wrath branch when it is added
+	urls["2"]="https://raw.githubusercontent.com/BigWigsMods/WoWUI/tbc/Interface/FrameXML/ChatFrame.lua"
 
 	for key in "${!urls[@]}"; do
 		echo ""
